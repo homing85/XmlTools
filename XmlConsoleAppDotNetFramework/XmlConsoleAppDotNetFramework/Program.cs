6426bd56475d007647d4b9e57d6a8e26d4b34049
homing85 <homing85@gmail.com>
2024-04-10 09:08:56 +0200

    We gaan een deel vervangen door vanuit xml gegenereerde xsds.

--- XmlConsoleAppDotNetFramework/XmlConsoleAppDotNetFramework/Program.cs
+++ XmlConsoleAppDotNetFramework/XmlConsoleAppDotNetFramework/Program.cs
@@ -43,6 +43,7 @@ namespace XmlConsoleAppDotNetFramework
             XmlSerializer serializer2 = new XmlSerializer(typeof(pgt.FeatureCollectionType));
             pgt.FeatureCollectionType deserializeRequest2 = (pgt.FeatureCollectionType)serializer2.Deserialize(new XmlTextReader(exampleXmlPath2));
 
+            
             pgt.FeatureCollectionType pgt = null;
             try
             {
@@ -59,6 +60,41 @@ namespace XmlConsoleAppDotNetFramework
             }
 
             Console.WriteLine("lets create some classes from xsd first");
+
+            ////Create xsd using xml
+            //XmlReader reader2 = XmlReader.Create(@"C:\repository\Tests\TestOutput\TestBroCptActions\XSD\class_based_on_CPT_registrationRequest\CPT_registrationRequest.xml");
+            //XmlSchemaSet schemaSet = new XmlSchemaSet();
+            //XmlSchemaInference schema = new XmlSchemaInference();
+
+            //schemaSet = schema.InferSchema(reader2);
+            //XmlWriter writer;
+            //int count = 0;
+            //foreach (XmlSchema s in schemaSet.Schemas())
+            //{
+            //    writer = XmlWriter.Create((count++).ToString() + "_cpt.xsd");
+            //    s.Write(writer);
+            //    writer.Close();
+            //    Console.WriteLine("Done " + count);
+            //}
+            //reader2.Close();
+
+            //// Using a modified CPT_in_out, the modified parts comes from a class auto-generated from xsd's that are auto-generated from a fugro generated cpt xml.
+            //CPT_in_out_mod.RegistrationRequestType1 CPT_in_out_request_mod = null;
+            //try
+            //{
+            //    XmlSerializer serializer = new XmlSerializer(typeof(CPT_in_out_mod.RegistrationRequestType1));
+            //    using (XmlTextReader reader = new XmlTextReader(exampleCptXmlPath))
+            //    {
+            //        CPT_in_out_request_mod = (CPT_in_out_mod.RegistrationRequestType1)serializer.Deserialize(reader);
+            //    }
+
+            //}
+            //catch (Exception exception)
+            //{
+            //    //Do nothing
+            //}
+            //WriteToXml(CPT_in_out_request_mod, @"C:\repository\Tests\TestOutput\TestBroCptActions\documents\DKM119_000_serialized_mod.xml");
+
 
         }
     }
