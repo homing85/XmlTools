ae9b7b29adbaeb2cbabc566ec95ec8ec4830db6d
hoeming <hoeming.wong@prorail.nl>
2025-01-08 12:42:40 +0100

    laatste commit

--- XmlConsoleAppDotNetFramework/XmlConsoleAppDotNetFramework/Program.cs
+++ XmlConsoleAppDotNetFramework/XmlConsoleAppDotNetFramework/Program.cs
@@ -10,6 +10,7 @@ using pgt;
 using System.Xml.Schema;
 using System.IO;
 
+
 namespace XmlConsoleAppDotNetFramework
 {
     internal class Program
@@ -22,13 +23,13 @@ namespace XmlConsoleAppDotNetFramework
             // Test creation imx122
             string exampleXmlPath = @"C:\Repository\XmlTools\Voorbeelden\20220418_V28_F_Eindhoven_Venlo_D_OK0213.xml";
             // .
-            imx122.IMSpoor im122 = null;
+            imx123.IMSpoor im123 = null;
             try
             {
-                XmlSerializer serializer = new XmlSerializer(typeof(imx122.IMSpoor));
+                XmlSerializer serializer = new XmlSerializer(typeof(imx123.IMSpoor));
                 using (XmlTextReader reader = new XmlTextReader(exampleXmlPath))
                 {
-                    im122 = (imx122.IMSpoor)serializer.Deserialize(reader);
+                    im123 = (imx123.IMSpoor)serializer.Deserialize(reader);
                 }
 
             }
@@ -36,22 +37,27 @@ namespace XmlConsoleAppDotNetFramework
             {
                 //Do nothing
             }
-
+            //Crotec.PGT.Data.Model.PGT pgtmodel = new PGT();
+            //Crotec.PGT.Parser.PGTRootParser parser = new Crotec.PGT.Parser.PGTRootParser();
+            
             // Test creation imx122
             string exampleXmlPath2 = @"C:\Repository\XmlTools\Voorbeelden\IMGeo_Naiade_Export_17_08_2022T12_42_33.xml";
             // .
+            
+            //pgt2.FeatureCollectionType MyObj = DeserializeXMLFileToObject<pgt2.FeatureCollectionType>(exampleXmlPath2);
 
-            XmlSerializer serializer2 = new XmlSerializer(typeof(pgt.FeatureCollectionType));
-            pgt.FeatureCollectionType deserializeRequest2 = (pgt.FeatureCollectionType)serializer2.Deserialize(new XmlTextReader(exampleXmlPath2));
+
+            XmlSerializer serializer2 = new XmlSerializer(typeof(pgt2.FeatureCollectionType));
+            pgt2.FeatureCollectionType deserializeRequest2 = (pgt2.FeatureCollectionType)serializer2.Deserialize(new XmlTextReader(exampleXmlPath2));
 
             
-            pgt.FeatureCollectionType pgt = null;
+            pgt2.FeatureCollectionType pgt = null;
             try
             {
-                XmlSerializer serializer = new XmlSerializer(typeof(pgt.FeatureCollectionType));
+                XmlSerializer serializer = new XmlSerializer(typeof(pgt2.FeatureCollectionType));
                 using (XmlTextReader reader = new XmlTextReader(exampleXmlPath2))
                 {
-                    pgt = (pgt.FeatureCollectionType)serializer.Deserialize(reader);
+                    pgt = (pgt2.FeatureCollectionType)serializer.Deserialize(reader);
                 }
 
             }
@@ -59,11 +65,13 @@ namespace XmlConsoleAppDotNetFramework
             {
                 //Do nothing
             }
-
+ 
             Console.WriteLine("lets create some classes from xsd first");
 
+
+            
             ////create xsd using xml
-            //XmlReader reader2 = XmlReader.Create(@"C:\Repository\XmlTools\Voorbeelden\IMGeo_Naiade_Export_17_08_2022T12_42_33.xml");
+            //XmlReader reader2 = XmlReader.Create(@"C:\Repository\blok61_62_47\62\IMGeo_Naiade_Export_31_08_2022T09_17_25.xml");
             //XmlSchemaSet schemaset = new XmlSchemaSet();
             //XmlSchemaInference schema = new XmlSchemaInference();
 
@@ -79,26 +87,42 @@ namespace XmlConsoleAppDotNetFramework
             //}
             //reader2.Close();
 
-            // using a modified cpt_in_out, the modified parts comes from a class auto-generated from xsd's that are auto-generated from a fugro generated cpt xml.
-            selfpgt.FeatureCollection selfpgt = null;
-            try
-            {
-                XmlSerializer serializer = new XmlSerializer(typeof(selfpgt.FeatureCollection));
-                using (XmlTextReader reader = new XmlTextReader(exampleXmlPath2))
-                {
-                    selfpgt = (selfpgt.FeatureCollection)serializer.Deserialize(reader);
-                }
+            //// using a modified cpt_in_out, the modified parts comes from a class auto-generated from xsd's that are auto-generated from a fugro generated cpt xml.
+            //selfpgt.FeatureCollection selfpgt = null;
+            //try
+            //{
+            //    XmlSerializer serializer = new XmlSerializer(typeof(selfpgt.FeatureCollection));
+            //    using (XmlTextReader reader = new XmlTextReader(exampleXmlPath2))
+            //    {
+            //        selfpgt = (selfpgt.FeatureCollection)serializer.Deserialize(reader);
+            //    }
 
-            }
-            catch (Exception exception)
-            {
-                //Do nothing
-            }
-            WriteToXml(selfpgt, @"C:\Repository\XmlTools\Voorbeelden\IMGeo_Naiade_Export_17_08_2022T12_42_33_test2.xml");
+            //}
+            //catch (Exception exception)
+            //{
+            //    //Do nothing
+            //}
+            //WriteToXml(selfpgt, @"C:\Repository\XmlTools\Voorbeelden\IMGeo_Naiade_Export_17_08_2022T12_42_33_test2.xml");
 
 
         }
+        public static T DeserializeXMLFileToObject<T>(string XmlFilename)
+        {
+            T returnObject = default(T);
+            if (string.IsNullOrEmpty(XmlFilename)) return default(T);
 
+            try
+            {
+                StreamReader xmlStream = new StreamReader(XmlFilename);
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                returnObject = (T)serializer.Deserialize(xmlStream);
+            }
+            catch (Exception ex)
+            {
+                // do nothing
+            }
+            return returnObject;
+        }
         private static void WriteToXml<T>(T registrationRequest, string v)
         {
             using (Stream stream = File.Open(v, FileMode.Create))
